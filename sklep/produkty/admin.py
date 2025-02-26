a046846d199fe8e4f81371a76f0def7d17a91ff8
Jakub Bocheński <kuba.bochenski@vp.pl>
2025-02-26 12:17:38 +0100

    Dodanie formularza do dodawania produktów

--- sklep/produkty/admin.py
+++ sklep/produkty/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Produkt
+from django.utils.html import format_html
 
-admin.site.register(Produkt)
+class ProduktAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'cena', 'link_do_strony')
+
+    def link_do_strony(self, obj):
+        return format_html('<a href="{}">Zobacz na stronie</a>', f'/{obj.id}/')
+
+    link_do_strony.short_description = 'Link do strony produktu'
+
+admin.site.register(Produkt, ProduktAdmin)
--- sklep/produkty/urls.py
+++ sklep/produkty/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lista_produktow
+from .views import dodaj_produkt, lista_produktow
 
 urlpatterns = [
-    path('', lista_produktow, name='lista_produktow'),
+    path('', lista_produktow, name='lista_produktow'), 
+    path('dodaj/', dodaj_produkt, name='dodaj_produkt'),
 ]
