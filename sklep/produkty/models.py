d0a54f114764720923a0fbc52a848e7618d58cb9
Jakub Bocheński <kuba.bochenski@vp.pl>
2025-02-26 11:56:08 +0100

    Dodano model Produkt do panelu admina

--- sklep/produkty/models.py
+++ sklep/produkty/models.py
@@ -6,4 +6,11 @@ class Produkt(models.Model):
 
     def __str__(self):
         return self.nazwa
+from django.db import models
 
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=100, verbose_name='Nazwa produktu')
+    cena = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Cena produktu')
+
+    def __str__(self):
+        return f'{self.nazwa} - {self.cena}'
