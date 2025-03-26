591af674bbf47fea6322f9ab4490fe8925b2c2fe
Jakub Bocheński <kuba.bochenski@vp.pl>
2025-03-26 11:58:29 +0100

    Dodano logowanie

--- sklep/produkty/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Produkt(models.Model):
-    nazwa = models.CharField(max_length=100)
-    cena = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f'{self.nazwa} - {self.cena} PLN'
--- sklep/produkty/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import dodaj_produkt, lista_produktow
-
-urlpatterns = [
-    path('', lista_produktow, name='lista_produktow'), 
-    path('dodaj/', dodaj_produkt, name='dodaj_produkt'),
-]
